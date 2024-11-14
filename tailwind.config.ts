7095e653b4ab4369b88d875204679150b696a006
Leticia Nobre <leticiavidal2910@gmail.com>
2024-11-14 20:39:52 -0300

    🔧 refat: adjusted talwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
     extend: {
       colors: {
         background: "var(--background)",
