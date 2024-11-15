da6ffc6a6aae408c02ded63cc887fd209064b630
Leticia Nobre <leticiavidal2910@gmail.com>
2024-11-14 21:57:57 -0300

    💄 style: button color

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -30,7 +30,7 @@ const Sobre = () => {
                         <p className="mt-3 text-gray-600">
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
                         </p>
-                        <a href="javascript:void(0)" className="inline-flex border border-blue-200 rounded-xl py-2 px-4 gap-x-1 items-center text-indigo-600 hover:text-indigo-500 duration-150 font-medium">
+                        <a href="javascript:void(0)" className="inline-flex border border-blue-200 rounded-xl py-2 px-4 gap-x-1 items-center text-blue-800 hover:text-blue-600 hover:border-blue-600 duration-150 font-medium">
                             Explore more
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                 <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd" />
