a86edc59a9eeaf2b407ac20dd5d0d8aaea966133
Leticia Nobre <leticiavidal2910@gmail.com>
2024-11-14 21:27:21 -0300

    ✨ feat: added images to hero section

--- src/pages/hero/index.tsx
+++ src/pages/hero/index.tsx
@@ -1,7 +1,7 @@
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
 const Hero = () => {
-
     const [state, setState] = useState(false)
 
     // Replace javascript:void(0) paths with your paths
@@ -11,7 +11,6 @@ const Hero = () => {
         { title: "Produtos", path: "javascript:void(0)" },
         { title: "Preços", path: "javascript:void(0)" }
     ]
-
 
     const Brand = () => (
         <div className="flex items-center justify-between py-5 md:block">
@@ -44,27 +43,23 @@ const Hero = () => {
     )
 
     return (
-        <div className='relative min-h-screen'>
+        <div className='relative min-h-screen w-full'>
             <header>
                 <div className={`md:hidden ${state ? "mx-2 pb-5" : "hidden"}`}>
                     <Brand />
                 </div>
-                <nav className={`pb-5 md:text-sm ${state ? "absolute top-0 inset-x-0 bg-white shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-0 md:mt-0 md:relative md:bg-transparent" : ""}`}>
+                <nav className={`md:text-sm ${state ? "absolute top-0 inset-x-0 bg-white shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-0 md:mt-0 md:relative md:bg-transparent" : ""}`}>
                     <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
                         <Brand />
                         <div className={`flex-1 items-center mt-8 md:mt-0 md:flex ${state ? 'block' : 'hidden'} `}>
                             <ul className="flex-1 justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
-                                {
-                                    navigation.map((item, idx) => {
-                                        return (
-                                            <li key={idx} className="text-gray-700 hover:text-gray-900">
-                                                <a href={item.path} className="block">
-                                                    {item.title}
-                                                </a>
-                                            </li>
-                                        )
-                                    })
-                                }
+                                {navigation.map((item, idx) => (
+                                    <li key={idx} className="text-gray-700 hover:text-gray-900">
+                                        <a href={item.path} className="block">
+                                            {item.title}
+                                        </a>
+                                    </li>
+                                ))}
                             </ul>
                             <div className="items-center justify-end mt-6 space-y-6 md:flex md:mt-0">
                                 <a href="javascript:void(0)" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
@@ -78,48 +73,33 @@ const Hero = () => {
                     </div>
                 </nav>
             </header>
-            <section>
-                <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 overflow-hidden md:px-8 md:flex">
-                    <div className='flex-none space-y-5 max-w-xl'>
-                        <a href="javascript:void(0)" className='inline-flex gap-x-6 items-center rounded-full p-1 pr-6 border text-sm font-medium duration-150 hover:bg-white'>
-                            <span className='inline-block rounded-full px-3 py-1 bg-indigo-600 text-white'>
-                                News
-                            </span>
-                            <p className='flex items-center'>
-                                Read the launch post from here
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                    <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                                </svg>
-                            </p>
-                        </a>
-                        <h1 className="text-4xl text-gray-800 font-extrabold sm:text-5xl">
-                            Build your SaaS exactly how you want
-                        </h1>
-                        <p>
-                            Sed ut perspiciatis unde omnis iste natus voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-                        </p>
-                        <div className='flex items-center gap-x-3 sm:text-sm'>
-                            <a href="javascript:void(0)" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
-                                Get started
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                    <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                                </svg>
-                            </a>
-                            <a href="javascript:void(0)" className="flex items-center justify-center gap-x-1 py-2 px-4 text-gray-700 hover:text-gray-900 font-medium duration-150 md:inline-flex">
-                                Contact sales
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                    <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                    <div className='flex-1 hidden md:block'>
-                        {/* Replace with your image */}
-                        <img src="https://raw.githubusercontent.com/sidiDev/remote-assets/c86a7ae02ac188442548f510b5393c04140515d7/undraw_progressive_app_m-9-ms_oftfv5.svg" className="max-w-xl" />
-                    </div>
-                </div>
-            </section>
+            <section className="flex flex-col items-center justify-center min-h-[calc(100vh-80px)]">
+    <div className="flex flex-col items-center justify-center">
+        <div className='absolute z-50'>
+            <Image 
+                src={'/donutshome.svg'} 
+                alt='donuts home image' 
+                layout="responsive"
+                width={450} 
+                height={100} 
+                className="w-[70%] max-w-[450px] sm:w-[50%] md:w-[40%] lg:w-[30%]"
+            />
+        </div>
+        <div className='relative'>
+            <Image 
+                src={'/2023.svg'} 
+                alt='2023 image'
+                layout="responsive"
+                width={900} 
+                height={100} 
+                className="w-[80%] max-w-[900px] sm:w-[60%] md:w-[50%] lg:w-[40%]"
+            />
+        </div>
+    </div>
+</section>
+
         </div>
     )
 }
+
 export default Hero;
