a69de0b4acee8082bf757cce4c32244050a84a46
Leticia Nobre <leticiavidal2910@gmail.com>
2024-11-14 21:58:32 -0300

    ♻️ refat: changed title

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -7,7 +7,7 @@ const Sobre = () => {
                 <div className="max-w-screen-xl mx-auto px-4 md:text-center md:px-8">
                     <div className="max-w-xl space-y-3 md:mx-auto">
                         <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-                            Build the future with us
+                            Assorted Donuts
                         </p>
                         <p className="text-gray-600">
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident.
