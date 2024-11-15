6308b8d622a303bd04b4b8f8e71ee1ad047d0694
Leticia Nobre <leticiavidal2910@gmail.com>
2024-11-15 15:04:38 -0300

    ✨ feat: added cards and images

--- src/pages/produtos/index.tsx
+++ src/pages/produtos/index.tsx
@@ -1,28 +1,30 @@
+import Image from "next/image";
+
 const Produtos = () => {
 
     const posts = [
         {
-            title: "What is SaaS? Software as a Service Explained",
-            desc: "Going into this journey, I had a standard therapy regimen, based on looking at the research literature. After I saw the movie, I started to ask other people what they did for their anxiety, and some",
-            img: "https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+            title: "Blueberry Confetti",
+            desc: "R$ 9.00",
+            img: "/product1.jpg",
             href: "javascript:void(0)"
         },
         {
-            title: "A Quick Guide to WordPress Hosting",
-            desc: "According to him, â€œI'm still surprised that this has happened. But we are surprised because we are so surprised.â€More revelations about Whittington will be featured in the film",
-            img: "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+            title: "Strawberry Confetti",
+            desc: "R$ 12.00",
+            img: "/product2.jpg",
             href: "javascript:void(0)"
         },
         {
-            title: "7 Promising VS Code Extensions Introduced in 2022",
-            desc: "I hope I remembered all the stuff that they needed to know. They're like, 'okay,' and write it in their little reading notebooks. I realized today that I have all this stuff that",
-            img: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+            title: "Cream Peanut",
+            desc: "R$ 10.00",
+            img: "/product3.jpg",
             href: "javascript:void(0)"
         },
     ]
     
     return (
-        <section className="mt-12 mx-auto px-4 max-w-screen-xl md:px-8">
+        <section className="flex flex-col items-center justify-center mx-auto px-4 max-w-screen-xl md:px-8">
             <div className="text-center">
                 <h1 className="text-3xl text-gray-800 font-semibold">
                     Take a doughnut break
@@ -31,20 +33,26 @@ const Produtos = () => {
             <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
                 {
                     posts.map((items, key) => (
-                        <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
+                        <div className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
                             <a href={items.href}>
-                                <img src={items.img} loading="lazy" alt={items.title}  className="w-full h-50 rounded-t-md" />
+                                <Image src={items.img} alt={items.title}  width={300} height={200} className="w-full  h-[330px] object-cover rounded-t-md" />
                                 <div className="pt-3 ml-4 mr-2 mb-3">
                                     <h3 className="text-xl text-gray-900">
                                         {items.title}
                                     </h3>
-                                    <p className="text-gray-400 text-sm mt-1">{items.desc}</p>
+                                    <p className="text-gray-400 text-xl mt-1">{items.desc}</p>
                                 </div>
                             </a>
-                        </article>
+                        </div>
                     ))
                 }
             </div>
+            <a href="javascript:void(0)" className="inline-flex mt-12 border border-blue-200 rounded-xl py-2 px-4 gap-x-1 items-center text-gray-500 hover:text-orange-600 hover:border-orange-600 duration-150 font-medium">
+                Explore more
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd" />
+                </svg>
+            </a>
         </section>
     )
 }
